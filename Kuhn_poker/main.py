1c37dcb31c5709fc9da93227c64705847d7c2995
lukearcus <rickard@robots.ox.ac.uk>
2022-08-04 17:08:34 +0100

    tidied up main

--- Kuhn_poker/main.py
+++ Kuhn_poker/main.py
@@ -4,32 +4,6 @@ import learners
 import matplotlib.pyplot as plt
 from scipy.ndimage.filters import gaussian_filter1d
 
-def gen_belief(p1, p2):
-    belief_1 = np.zeros([6,3])+1
-    belief_2 = np.zeros([6,3])+1
-    for i in range(10000):
-        game.start_game()
-        player=p1
-        while not game.ended:
-            player.observe(game.observe())
-            game.action(player.action())
-            if player == p1:
-                player = p2
-            else:
-                player = p1
-        rewards = game.end_game()
-        c_1 = game.cards[0] - 1
-        c_2 = game.cards[1] - 1
-        for s in p1.state_hist:
-            belief_1[s,c_2] += 1
-        for s in p2.state_hist:
-            belief_2[s,c_1] += 1
-        p1.reset()
-        p2.reset()
-    belief_1 /= np.sum(belief_1,1,keepdims=True)
-    belief_2 /= np.sum(belief_2,1,keepdims=True)
-    return belief_1, belief_2
-
 
 def play_game(players, game):
     game.start_game()
@@ -45,75 +19,6 @@ def play_game(players, game):
     for player in players:
         player.reset()
     return reward
-
-def play_obl_game(p1, p2, game):
-    """
-    This is a mess, fix at some point
-    """
-    game.start_game()
-    player=p1
-    while not game.ended:
-        player.observe(game.observe())
-        game.action(player.action())
-        if player == p1:
-            player = p2
-        else:
-            player = p1
-    rewards = game.end_game()
-    if game.curr_bets[0] == game.curr_bets[1]:
-        if p1.belief is not None:
-            for state in p1.state_hist: 
-                probs = p1.belief[state]
-                fict = np.argmax(np.random.multinomial(1, pvals = probs))
-                p1.state = state
-                act = p1.action()
-                if state < 3:
-                    if act == "bet":
-                        p2.state = 2+fict
-                        p2_act = p2.action()
-                        if p2_act == "bet":
-                            if game.cards[0] > fict:
-                                p1.Q_update(state, p1.act, 2, -1)
-                            else:
-                                p1.Q_update(state, p1.act, -2, -1)
-                        else:
-                            p1.Q_update(state, p1.act, 1, -1)
-                    else:
-                        p2.state = fict
-                        p2_act = p2.action()
-                        if p2_act == "bet":
-                            p1.Q_update(state, p1.act, 0, 3+state)
-                        else:
-                            if game.cards[0] > fict:
-                                p1.Q_update(state, p1.act, 1, -1)
-                            else:
-                                p1.Q_update(state, p1.act, -1, -1)
-                else:
-                    if act == "fold":
-                        p1.Q_update(state, p1.act, -1, -1)
-                    else:
-                        if game.cards[0] > fict:
-                            p1.Q_update(state, p1.act, 2, -1)
-                        else:
-                            p1.Q_update(state, p1.act, -2, -1)
-
-
-        else:
-            p1.get_reward(rewards[0])
-        if p2.belief is not None:
-            probs_2 = p2.belief[p2.state]
-            fict_s2 = np.argmax(np.random.multinomial(1, pvals = probs_2)) + 1
-            if game.cards[1] > fict_s2:
-                p2.get_reward(max(rewards))
-            else:
-                p2.get_reward(min(rewards))
-        else:
-            p2.get_reward(rewards[1])
-    else:
-        p1.get_reward(rewards[0])
-        p2.get_reward(rewards[1])
-    p1.reset()
-    p2.reset()
 
 game = Kuhn_Poker_int_io()
 num_lvls = 4
